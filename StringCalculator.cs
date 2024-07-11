036c4a9194f6d8ec14ea1ea18593c56f62beff05
Prajwal-R-007 <R.Prajwal2@in.bosch.com>
2024-07-11 10:57:25 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,45 @@
+using System;
+
+public class StringCalculator
+{
+    public int Add(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        string[] delimiters = { ",", "\n", "\\", ";", "//" };
+        string[] numbers = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        int total = 0;
+
+        foreach (string number in numbers)
+        {
+            int parsedNumber = ParseNumber(number);
+            if (parsedNumber < 1000)
+            {
+                total += parsedNumber;
+            }
+        }
+
+        return total;
+    }
+
+    private int ParseNumber(string number)
+    {
+        if (int.TryParse(number, out int result))
+        {
+            if (result < 0)
+            {
+                throw new Exception("Negative numbers are not allowed: " + result);
+            }
+            return result;
+        }
+        else
+        {
+            throw new Exception("Invalid input: " + number);
+        }
+    }
+}
 
